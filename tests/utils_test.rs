c09227a236250b564f055214038c58b1c52390c9
Graham Ullrich <graham@flyingcracker.com>
2024-11-01 23:44:39 -0600

    add test showing result with bad timestamp string

--- tests/utils_test.rs
+++ tests/utils_test.rs
@@ -2,6 +2,8 @@ use chrono::{DateTime, Utc};
 use sindri_scroll_sdk::prover::{SindriProofInfoResponse, SindriTaskStatus};
 use sindri_scroll_sdk::utils::proving_timestamps_from_response;
 
+// Verify results for `created_at`, `started_at`, and `finished_at` when the
+// response contains valid values.
 #[test]
 fn test_proving_timestamps_from_response() {
     let compute_time_sec: f64 = 10.0;
@@ -32,4 +34,31 @@ fn test_proving_timestamps_from_response() {
         finished_at.unwrap(),
         create_time_timestamp + queue_time_sec + compute_time_sec
     );
+}
+
+// Verify results for `created_at`, `started_at`, and `finished_at` when the
+// date_created string is invalid.
+#[test]
+fn test_proving_timestamps_from_response_invalid_datestring() {
+    let compute_time_sec: f64 = 10.0;
+    let date_str: &str = "2024-10-30:12:34:56.000Z"; // malformed date string
+    let queue_time_sec: f64 = 5.0;
+    let response: SindriProofInfoResponse = SindriProofInfoResponse {
+        compute_time_sec: Some(compute_time_sec),
+        date_created: date_str.to_string(),
+        error: None,
+        proof: None,
+        proof_id: "proof_id".to_string(),
+        queue_time_sec: Some(queue_time_sec),
+        status: SindriTaskStatus::Success,
+
+        verification_key: None,
+    };
+
+    // We expect no valid values.
+    let (created_at, started_at, finished_at) = proving_timestamps_from_response(&response);
+
+    assert_eq!(created_at, 0.0);
+    assert_eq!(started_at, None);
+    assert_eq!(finished_at, None);
 }
